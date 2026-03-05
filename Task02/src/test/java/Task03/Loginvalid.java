2986ccc0ab2ae052c59b08339f8f70baa17c8ace
mohdejas <mohamedejas612@gmail.com>
2026-03-05 13:37:16 +0530

    Internship Project Task 3

---
+++ Task02/src/test/java/Task03/Loginvalid.java
@@ -0,0 +1,25 @@
+package Task03;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Loginvalid {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.saucedemo.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.id("login-button")).click();
+		
+		
+			
+		
+
+	}
+}
+
